1f7e1deeccbe08b8b398dd52861e2197c6a8e344
jpcoelhosenac <jpcoelho@senacrs.com.br>
2025-03-24 16:56:52 -0300

    Inserindo hqs 1, 2, 3 e 4.

---
+++ 04_interacao_gestao/oda/hq/js/tirinha-vertical.js
@@ -0,0 +1,140 @@
+var hq1_txt = [
+    "A Delícias Saudáveis é uma empresa especializada na produção de alimentos congelados, saudáveis e orgânicos. Está no mercado desde 2010 e visa produzir e fornecer alimentos saudáveis e saborosos utilizando práticas sustentáveis e éticas para a promoção de saúde e bem-estar aos seus clientes.",
+    "Dois anos após a criação da “Delícias Saudáveis” ocorreu a contratação de um técnico em qualidade, chamado Thiago, para garantir que os produtos atendam às expectativas e aos requisitos de seus clientes. Quando iniciou o seu trabalho, Thiago utilizou indicadores e metrologia para criar a sua rotina de trabalho.",
+    "Thiago, ao iniciar seu trabalho na “Delícias Saudáveis”, definiu vários indicadores-chave de qualidade que seriam relevantes para a empresa. Além da taxa de defeitos, o tempo de ciclo de produção e a eficiência do processo, ele também considerou os seguintes indicadores:",
+    "Após essa etapa, quando um indicador de qualidade não atingia a meta estabelecida, Thiago entrava em ação. Por exemplo, se o teor de sódio em um lote de produtos estava acima do permitido, ele investigava a causa raiz do problema. Isso envolvia a análise do processo de produção, como a quantidade de sal adicionada durante a fabricação.",
+    "Thiago procurava cumprir com as calibrações periódicas (em intervalos regulares conforme o calendário de calibração que ele montou), mas, quando identificava um problema, como uma balança que não estava calibrada corretamente, ele se encarregava da verificação intermediária do equipamento (que é realizada na própria empresa e geralmente pela equipe operacional ou pelo técnico responsável). Nesse caso, o técnico em qualidade mantém um registro documentado de todas as ações.",
+    "Ele também implementou novas práticas de trabalho, como a introdução de verificações de qualidade mais frequentes e a implementação de um novo protocolo de treinamento para a equipe de produção.",
+    "Depois de implementar as ações corretivas, o trabalho de Thiago não parava por aí. Ele se dedicava ao monitoramento contínuo dos resultados para garantir que a melhoria de qualidade desejada fosse alcançada. Por exemplo, se uma balança fosse recalibrada para corrigir o teor de sódio nos produtos, Thiago coletaria e analisaria os dados de medição do teor de sódio nos lotes subsequentes para confirmar se a correção foi eficaz.",
+    "Além disso, Thiago, por meio de testes sensoriais, monitorava continuamente outros indicadores de qualidade, como a textura e o sabor dos produtos. Ele também usou ferramentas de metrologia, como espectrômetros, para medir a cor e a consistência dos alimentos.",
+    "Na Delícias Saudáveis, após a implementação das ações corretivas, o trabalho de Thiago não terminava. Ele se dedicava ao monitoramento contínuo dos resultados para garantir que as melhorias desejadas na qualidade fossem alcançadas. Por exemplo, se uma ação corretiva envolvesse a alteração da quantidade de um ingrediente em uma receita, Thiago coletaria amostras de lotes subsequentes do produto e realizaria testes para verificar se a alteração resultou na melhoria desejada.",
+    "O profissional também orientava a utilização de equipamentos de medição para coletar dados sobre diferentes aspectos dos produtos, como o teor de sódio, a textura e a cor. Esses dados eram então analisados para verificar se estavam dentro dos padrões de qualidade estabelecidos.",
+    "Para atingir os resultados dos quais foram propostos em conjunto com os diretores da “Delícias Saudáveis”, Thiago sabia que o mapeamento de processos lhe traria maiores informações sobre a indústria e que seu trabalho poderia ser qualificado. <br><br>Vamos conhecer a importância da metrologia no mapeamento dos processos e seguir acompanhando a trajetória de Thiago na Delícias Saudáveis. Continua..."
+]
+
+var table_indicadores = `
+ <div class="table-responsive table">
+                            <table class="text-left align-middle">
+                                <thead>
+                                    <tr>
+                                        <th class="w-25">Indicador</th>
+                                        <th class="w-25">Descrição</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td class="w-25">Eficiência Global dos Equipamentos (OEE)</td>
+                                        <td class="w-25">Mede a eficiência geral dos equipamentos utilizados na
+                                            produção. </td>
+                                    </tr>
+                                    <tr>
+                                        <td>Horas de trabalho na produção</td>
+                                        <td>Mede a quantidade de tempo gasto na produção.</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Número de produtos produzidos</td>
+                                        <td>Mede a quantidade de produtos produzidos em um determinado período.</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Tempo Médio de Reparo (MTTR)</td>
+                                        <td>Mede o tempo médio necessário para reparar equipamentos ou resolver
+                                            problemas de produção.</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Utilização da capacidade instalada</td>
+                                        <td>Mede a porcentagem da capacidade de produção utilizada na “Delícias
+                                            Saudáveis”.</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Indicadores de eficácia</td>
+                                        <td>Indica se os processos estão alcançando os objetivos propostos.</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Indicador de atendimento</td>
+                                        <td>Indica a satisfação dos clientes em relação aos produtos e aos serviços
+                                            da “Delícias saudáveis”.</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <p class="figure-caption text-center">Exemplos de indicadores de metrologia <br>
+                            Fonte: Senac EAD (2024)
+                        </p>
+`;
+var hq2_txt = [
+    "Thiago, ao iniciar as suas atividades na Delícias Saudáveis, entendeu que era de extrema importância mapear todos os processos envolvidos na produção de alimentos. Isso incluiu processos como recebimento de matérias-primas, armazenamento, preparação, cozimento, embalagem, congelamento e distribuição.",
+    "Depois de identificar os processos, o técnico pôde desenhar um diagrama de fluxo de processos que mostra como cada processo se conecta ao próximo. Isso ajudou Thiago a visualizar a sequência de operações e a interação entre os processos.",
+    "O próximo passo de Thiago foi identificar os pontos de controle críticos em cada processo. Estes foram os pontos nos quais as medições precisaram ser feitas para garantir a qualidade dos produtos. Foi neste momento que a metrologia assumiu um papel de destaque.",
+    "Em cada ponto de controle, o técnico determinou quais medições precisavam ser feitas, quais instrumentos seriam usados e como os dados seriam coletados e analisados. Por exemplo, na Delícias Saudáveis, Thiago incluiu medições de temperatura, pH, umidade e de outros fatores que poderiam afetar a qualidade do produto. Ele indicou a utilização de um termômetro para medir a temperatura do forno e outro para os congeladores, um cronômetro para monitorar o tempo de cozimento e um medidor de umidade para verificar a qualidade da matéria-prima.",
+    "Com base nas medições, Thiago criou indicadores de qualidade. Por exemplo, ele estabeleceu que a temperatura do forno deve estar entre 180 °C e 200 °C e o tempo de cozimento deve ser de 20 a 25 minutos para garantir que os alimentos sejam cozidos adequadamente. Além disso, a farinha deve ter uma umidade abaixo de 14% para garantir a qualidade das formulações.",
+    "A partir dos dados coletados pela metrologia, Thiago conseguiu analisar e identificar áreas de melhoria, o que o auxiliou em ajustes nos processos para melhorar a eficiência e a qualidade dos produtos. Por exemplo, ele notou que a temperatura do forno variava muito, o que afetava a qualidade dos alimentos. Como resultado, o profissional recomendou a compra de um novo forno com melhor controle de temperatura.",
+    "Apesar de um longo trabalho desenvolvido, ainda era necessário criar estratégias para o controle e monitoramento dos processos. Continua..."
+]
+var hq3_txt = [
+    "Thiago começou a implementar a metrologia nos processos de produção da Delícias Saudáveis. Ele identificou os pontos críticos de controle em cada processo em que as medições precisavam ser feitas. Por exemplo, na linha de produção de lasanhas congeladas, ele identificou que a temperatura de congelamento e o tempo de cozimento eram críticos para a qualidade e a segurança do produto.",
+    "Então Thiago selecionou os instrumentos de medição adequados para cada ponto de controle. Ele escolheu um termômetro de alta precisão para medir a temperatura de congelamento e um cronômetro para monitorar o tempo de cozimento.",
+    "Com base nas normas de qualidade e segurança alimentar, Thiago estabeleceu limites de controle para cada medição. Por exemplo, ele determinou que a temperatura de congelamento deveria ser mantida abaixo de -18°C e o tempo de cozimento deveria ser de 20 a 25 minutos.",
+    "Thiago implementou um sistema de monitoramento contínuo para acompanhar as medições em tempo real. Se alguma medição saísse dos limites de controle, um alerta seria acionado e ações corretivas seriam tomadas imediatamente. Por exemplo, se a temperatura de congelamento subisse acima de -18 °C, o sistema de refrigeração seria ajustado para trazer a temperatura de volta ao intervalo aceitável.",
+    "O técnico em qualidade usou os dados coletados para analisar o desempenho dos processos e identificar oportunidades de melhoria. Ele notou que a temperatura de congelamento tendia a subir durante certas horas do dia e, com base nessa informação, ajustou o cronograma de produção para evitar essas horas e melhorar a qualidade dos congelados.",
+    "Esse fato foi importante para identificar que a produção de alimentos da Delícias Saudáveis poderia gerar riscos para a saúde do consumidor, então Thiago passou a avaliar e mitigar riscos. Continua..."
+]
+var hq4_txt = [
+    "Thiago começou identificando os riscos potenciais em cada processo de produção. Por exemplo, ele reconheceu que uma falha no sistema de refrigeração poderia resultar em temperaturas de congelamento inadequadas, colocando em risco a qualidade e a segurança dos alimentos.",
+    "Em seguida, Thiago avaliou a probabilidade e o impacto de cada risco, utilizando a metrologia para coletar dados e realizar essa avaliação. Por exemplo, ele monitorou a temperatura de congelamento ao longo do tempo para determinar a frequência e a gravidade das variações de temperatura.",
+    "Com base na avaliação de riscos, Thiago desenvolveu controles para mitigar cada risco. Por exemplo, ele implementou um sistema de alarme que seria acionado se a temperatura de congelamento subisse acima do limite aceitável. Esse sistema permitiu uma resposta rápida para corrigir o problema e minimizar o impacto.",
+    "Thiago usou a metrologia para monitorar continuamente os riscos e a eficácia dos controles de risco. Ele coletou e analisou dados regularmente para identificar tendências e fazer ajustes conforme necessário. Caso ele notasse que o sistema de alarme estava sendo acionado com frequência, ele poderia investigar a causa e ajustar o sistema de refrigeração para melhorar a estabilidade da temperatura.",
+    "Por fim, Thiago revisou regularmente o plano de gestão de riscos e fez melhorias com base nos dados coletados e nas lições aprendidas. A metrologia forneceu a ele os dados necessários para fazer essas revisões e melhorias de forma informada e eficaz.",
+    "As estratégias de gestão de riscos imple mentadas por Thiago tiveram um impacto significativo na redução dos riscos na Delícias Saudáveis. A gestão proativa de riscos minimizou as interrupções na produção, maximizando a eficiência operacional e garantindo a continuidade dos negócios. Por exemplo, o sistema de alarme implementado por Thiago para monitorar a temperatura de congelamento permitiu uma resposta rápida a variações de temperatura, evitando interrupções na produção.",
+    "Além disso, as estratégias de gestão de riscos ajudaram a garantir a qualidade do produto em todo o seu ciclo de existência. A implementação de controles rigorosos de temperatura e tempo de cozimento resultou em produtos congelados de alta qualidade que atendiam consistentemente aos padrões de segurança alimentar.",
+    "A gestão de riscos também levou a um ambiente de trabalho mais seguro. Ao identificar e mitigar riscos, como falhas no equipamento ou erros humanos, Thiago foi capaz de reduzir a probabilidade de acidentes de trabalho.",
+    "Por sua vez, ao evitar interrupções na produção e melhorar a eficiência dos processos, as estratégias de gestão de riscos de Thiago também levaram a uma redução nos custos operacionais.",
+    "Ao garantir a qualidade e a segurança dos produtos, as estratégias de gestão de riscos de Thiago levaram a um aumento na satisfação do cliente. Por fim, as estratégias de gestão de riscos implementadas por Thiago tiveram um impacto positivo significativo na Delícias Saudáveis, melhorando a qualidade do produto, aumentando a segurança, reduzindo os custos e aumentando a satisfação do cliente, garantindo que os produtos sempre atendessem aos mais altos padrões de qualidade."
+]
+
+$(function () {
+    var hqTED = function (el, path, img_alt) {
+
+        $(el).each(function (index, element) {
+            let hq_id = "#" + $(this).attr("id");
+            let hq_radical = $(this).attr("data-radical");
+            let hq_quadro_inicio = $(this).attr("data-quadro-inicial");
+            let hq_quadro_fim = $(this).attr("data-quadro-final");
+
+            for (i = hq_quadro_inicio; i <= hq_quadro_fim; i++) {
+                let esconder = "";
+                let inserir_indicadores = "";
+                if (i > 1) esconder = "d-none";
+                if (i == 3 && hq_id == "#hq1") inserir_indicadores = table_indicadores;
+                $(hq_id + " .corpo").append('<div class="quadrinho quadrinho' + i + ' ' + esconder + '"><img src="' + path + '/' + hq_radical + i + '.png" class="img-fluid img' + i + '" alt="' + img_alt[i - 1] + '"><span>' + img_alt[i - 1] + '</span>' + inserir_indicadores + '</div>');
+            }
+            $(hq_id).attr("data-quadro-atual", "1");
+        })
+    }
+
+    $("body").on("click", ".btn-hq-proximo", function () {
+        let hq_id = "#" + $(this).parents(".hq").attr("id");
+        console.log(hq_id)
+        let hq_quadro_atual = $(hq_id).attr("data-quadro-atual");
+        let hq_quadro_fim = $(hq_id).attr("data-quadro-final");
+        if (hq_quadro_atual == (hq_quadro_fim - 1)) {
+            $(this).remove();
+     
+                
+        }
+        $(hq_id).attr("data-quadro-atual", ++hq_quadro_atual)
+        console.log(hq_id, hq_quadro_atual)
+        $(hq_id + " .corpo .quadrinho").eq(--hq_quadro_atual).removeClass("d-none");
+
+        $('html, body').animate({
+            scrollTop: $(hq_id + " .corpo .img" + ++hq_quadro_atual).offset().top
+        }, 'slow');
+
+    });
+
+    hqTED(".hq1", "oda/hq/img/hq1", hq1_txt);
+    hqTED(".hq2", "oda/hq/img/hq2", hq2_txt);
+    hqTED(".hq3", "oda/hq/img/hq3", hq3_txt);
+    hqTED(".hq4", "oda/hq/img/hq4", hq4_txt);
+
+
+});
